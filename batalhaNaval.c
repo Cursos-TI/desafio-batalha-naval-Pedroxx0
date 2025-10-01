c7bcec1c617a7757b71e64a39b98d074123d8280
Pedroxx0 <pedrohenrique_0001@hotmail.com>
2025-10-01 22:34:32 +0000

    Implementação no código

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 int main() {
-
     int tabuleiro[10][10];
     char linha[10] = {'A','B','C','D','E','F','G','H','I','J'};
 
@@ -12,14 +11,24 @@ int main() {
         }
     }
 
-    // Posiciona navio horizontal (linha C, colunas 5 a 7)
+    // Navio 1: horizontal (linha C, colunas 5 a 7)
     for(int j = 4; j < 7; j++) {
         tabuleiro[2][j] = 3;
     }
 
-    // Posiciona navio vertical (coluna 8, linhas F a H)
+    // Navio 2: vertical (coluna 8, linhas F a H)
     for(int i = 5; i < 8; i++) {
         tabuleiro[i][7] = 3;
+    }
+
+    // Navio 3: diagonal principal (tabuleiro[i][i], de A1 a E5)
+    for(int i = 0; i < 5; i++) {
+        tabuleiro[i][i] = 3;
+    }
+
+    // Navio 4: diagonal secundária (tabuleiro[i][9-i], de A10 a E6)
+    for(int i = 0; i < 5; i++) {
+        tabuleiro[i][9 - i] = 3;
     }
 
     // Imprime cabeçalho das colunas
